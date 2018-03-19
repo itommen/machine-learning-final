c6ca1ae4874498a2b5e6e70ed41d1dfd7702ed8f
itommen <itommen@gmail.com>
2018-03-19 08:37:34 -0800

    wip

--- cifar10/train-code.py
+++ cifar10/train-code.py
@@ -40,35 +40,64 @@ class PlotLosses(keras.callbacks.Callback):
         plt.plot(self.x, self.val_losses, label="val_loss")
         plt.legend()
         plt.show();
+
+def createModel():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same',
+                   input_shape=x_train.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    
+    return model
         
 plot_losses = PlotLosses()
 
-shrink_data = True
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
+# TODO: Should find the correct word ... 
+def calcXY(selected_classes, x_list, y_list):
+    x = [ex for ex, ey in zip(x_list, y_list) if ey in selected_classes]
+    y = [selected_classes.index(ey) for ex, ey in zip(x_list, y_list) if ey in selected_classes]
+    
+    return np.stack(x), np.stack(y).reshape(-1,1)
+
+def getCheckPoint():
+    filepath = 'cifar-bestmodel-ep{epoch:03d}-loss{loss:.3f}.h5'
+    return ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+shrink_data = True
 if shrink_data:
     selected_classes = [2, 3, 5, 6, 7]
     print('train\n', x_train.shape, y_train.shape)
-    x = [ex for ex, ey in zip(x_train, y_train) if ey in selected_classes]
-    y = [selected_classes.index(ey) for ex, ey in zip(x_train, y_train) if ey in selected_classes]
-    x_train = np.stack(x)
-    y_train = np.stack(y).reshape(-1,1)
+    
+    x_train, y_train = calcXY(selected_classes, x_train, y_train)
     print(x_train.shape, y_train.shape)
+    
     print('test\n', x_test.shape, y_test.shape)
-    x = [ex for ex, ey in zip(x_test, y_test) if ey in selected_classes]
-    y = [selected_classes.index(ey) for ex, ey in zip(x_test, y_test) if ey in selected_classes]
-
-    x_test = np.stack(x)
-    y_test = np.stack(y).reshape(-1,1)
+    x_test, y_test = calcXY(selected_classes, x_test, y_test)
     print(x_test.shape, y_test.shape)
     num_classes = len(selected_classes)
 else:
     print('train\n', x_train.shape, y_train.shape)
     print('test\n', x_test.shape, y_test.shape)
     num_classes = 5
-
-batch_size = 256
-epochs = 400
 
 # The data, shuffled and split between train and test sets:
 print('x_train shape:', x_train.shape)
@@ -79,28 +108,7 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-               input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Dropout(0.5))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
+model = createModel()
 
 # initiate RMSprop optimizer
 opt = keras.optimizers.rmsprop(lr=0.00015, decay=1e-6)
@@ -111,14 +119,13 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
 
-# define checkpoint callback
-filepath = 'cifar-bestmodel-ep{epoch:03d}-loss{loss:.3f}.h5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+checkpoint = getCheckPoint();
+
+batch_size = 256
+epochs = 1
 
 model.fit(x_train, y_train,  # this is our training examples & labels
           batch_size=batch_size,
@@ -128,6 +135,6 @@ model.fit(x_train, y_train,  # this is our training examples & labels
           callbacks=[plot_losses,checkpoint])  # this prints our loss at the end of every epoch
 
 # Score trained model.
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('==> Test loss:', scores[0])
-print('==> Test accuracy:', scores[1])
+loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
+print('# Test loss:', loss)
+print('# Test accuracy:', accuracy)
